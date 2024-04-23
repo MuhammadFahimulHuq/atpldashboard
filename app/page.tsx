e26c7d9379fc8de8edde60165ff6ed5bb4daf9bf
Muhammad Fahimul Huq <fahimul.huq85@gmail.com>
2024-04-23 16:45:48 +0600

    bug: reroute

--- app/page.tsx
+++ app/page.tsx
@@ -19,35 +19,44 @@ export default function Home() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const handleLogin = () => {
-    const user = User.User.find((u) => u.email === email);
-    if (!user) {
-      setError("Invalid email");
-      return;
-    }
-    bcrypt.compare(
-      password,
-      user.password,
-      (err: Error | null, result: boolean) => {
-        if (err || !result) {
-          setError("Invalid password");
-        } else {
-          // Successful login
-          localStorage.setItem("isLoggedIn", "true");
-          localStorage.setItem("username", user.username);
-          console.log("Logged in successfully!");
-          router.push("/dashboard");
-        }
+    try {
+      setLoading(true);
+      const user = User.User.find((u) => u.email === email);
+      if (!user) {
+        setError("Invalid email");
+        return;
       }
-    );
+      bcrypt.compare(
+        password,
+        user.password,
+        (err: Error | null, result: boolean) => {
+          if (err || !result) {
+            setError("Invalid password");
+          } else {
+            // Successful login
+            localStorage.setItem("isLoggedIn", "true");
+            localStorage.setItem("username", user.username);
+            console.log("Logged in successfully!");
+            router.push("/product");
+          }
+        }
+      );
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <main className="min-h-screen">
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center flex-col items-center h-screen px-2">
+        <img src="/atpl_logo.png" className="w-[100px]" alt="logo" />
+
         <Card>
           <CardHeader>
             <CardTitle className="font-bold">Login Now</CardTitle>
@@ -67,17 +76,33 @@ export default function Home() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
-              <Button type="button" className="mt-5" onClick={handleLogin}>
-                Sign in
+              <Button
+                type="button"
+                disabled={loading}
+                className="mt-5"
+                onClick={handleLogin}
+              >
+                {loading ? (
+                  <>
+                    <svg
+                      className="animate-spin h-5 w-5 mr-3 ..."
+                      viewBox="0 0 24 24"
+                    ></svg>
+                    Processing...
+                  </>
+                ) : (
+                  "Sign in"
+                )}
               </Button>
+
               {error && <p className="text-red-500 mt-2">{error}</p>}
             </div>
           </CardContent>
           <CardFooter className="flex flex-col">
-            <p className="text-sm">
+            <p className="text-xs">
               admin: admin@example.com | password: 12345{" "}
             </p>
-            <p className="text-sm">user: user@example.com | password: 12345 </p>
+            <p className="text-xs">user: user@example.com | password: 12345 </p>
           </CardFooter>
         </Card>
       </div>
